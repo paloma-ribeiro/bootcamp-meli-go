9ecc79603e7c496ef5e8cbda253f3111ab56a489
Paloma Ribeiro <palomaribeiro.dev@gmail.com>
2022-05-24 18:16:15 -0300

    Add files via upload

--- go-bases3/dia1/main.go
+++ go-bases3/dia1/main.go
@@ -1,3 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Exercício 1 - Guardar arquivo
+
+func guardarArquivo() {
+	produtosComprados := []byte("Banana; Maça; Laranja; Goiaba")
+	err := os.WriteFile("myFile.csv", produtosComprados, 0644)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(string(produtosComprados))
+	}
+}
+
+func main() {
+	guardarArquivo()
+}
